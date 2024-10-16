e05e6fb2c8ae6bf920fac6017e9703b43450a481
HarshavKoka <kokaharsha2203@gmail.com>
2024-10-17 00:39:12 +0530

    a typical project

---
+++ p.js
@@ -0,0 +1,41 @@
+const inputs = document.querySelectorAll("input"),
+    button = document.querySelector("button");
+
+inputs.forEach((input, index1) => {
+    input.addEventListener("keyup", (e) => {
+        const currentInput = input,
+            nextInput = input.nextElementSibling,
+            prevInput = input.previousElementSibling;
+        if (currentInput.value.length > 1) {
+            currentInput.value = "";
+            return;
+
+        }
+
+        if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
+            nextInput.removeAttribute("disabled");
+            nextInput.focus();
+        }
+        if (e.key === "Backspace") {
+            // iterate over all inputs again
+            inputs.forEach((input, index2) => {
+                // if the index1 of the current input is less than or equal to the index2 of the input in the outer loop
+                // and the previous element exists, set the disabled attribute on the input and focus on the previous element
+                if (index1 <= index2 && prevInput) {
+                    input.setAttribute("disabled", true);
+                    input.value = "";
+                    prevInput.focus();
+                }
+            });
+        }
+        //if the fourth input( which index number is 3) is not empty and has not disable attribute then
+        //add active class if not then remove the active class.
+        if (!inputs[3].disabled && inputs[3].value !== "") {
+            button.classList.add("active");
+            return;
+        }
+        button.classList.remove("active");
+    });
+});
+
+window.addEventListener("load", () => inputs[0].focus());
